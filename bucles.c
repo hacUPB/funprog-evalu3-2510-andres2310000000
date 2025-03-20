6016bf6543e569935c2d32edd78004d66579cdb2
andres gonzalez <andresd.gonzalez@upb.edu.co>
2025-03-20 09:32:43 -0500

    se realizo el quiz

---
+++ bucles.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a=0;
+    while (a<10)
+    {
+        a++;
+        printf("#:%d\n",a);
+    }
+    return 0;
+}
---
+++ xr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float n1,n2, r;
+    char operador;
+    int x;
+    do
+    {
+        printf("ingrese un numero, luego el operador, y luego el segundo numero");
+        printf("operaciones disponibles:\n+\n-\n*\n/\n ");
+        printf("")
+    } while (operador !`?`);
+    
+
+    switch(x)
+     {
+        case 1:
+            //x=1;
+            printf("selecciono opc 1\n");
+            break;
+        case 2:
+            x//=2
+            printf("selecciono opc 2\n");
+            break;
+        
+        default:
+            printf("no selecciono nada\n");
+            break;
+    }
+    return 0;
+}
+
